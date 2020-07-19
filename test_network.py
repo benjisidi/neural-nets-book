da668d080787d196d515796575777241cb4819de
Benji Sidi <benjisidi@gmail.com>
2020-07-19 17:05:28 +0100

    added timeit to test file

--- test_network.py
+++ test_network.py
@@ -1,9 +1,19 @@
 import mnist_loader
 from Network import Network
+from timeit import timeit
 
-training_data, validation_data, test_data = mnist_loader.load_data_wrapper(
-    "./mnist.pkl.gz"
-)
+
+def setup():
+    training_data, validation_data, test_data = mnist_loader.load_data_wrapper(
+        "./mnist.pkl.gz"
+    )
+    return (training_data, validation_data, test_data)
+
+
+def train(training_data, test_data):
+    testNet = Network([784, 30, 10])
+    testNet.train(training_data, 30, 10, 3.0, test_data=test_data)
+
 
 # # Debug: take a look at the first input image to ensure it has loaded correctly
 # import numpy as np
@@ -12,5 +22,11 @@ training_data, validation_data, test_data = mnist_loader.load_data_wrapper(
 # plt.imshow(im)
 # plt.show()
 
-testNet = Network([784, 30, 10])
-testNet.train(training_data, 30, 10, 3.0, test_data=test_data)
+if __name__ == "__main__":
+    print(
+        timeit(
+            "train(training_data, test_data)",
+            "from __main__ import setup, train\ntraining_data, validation_data, test_data = setup()",
+            number=1,
+        )
+    )
